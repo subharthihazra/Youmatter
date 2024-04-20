35bca505969734548f66e82745d9ca82d6060e45
Afeefuddin <afeefud2004din@gmail.com>
2024-04-21 00:49:02 +0530

    Email Service Fixed

--- Backend/gemini/getArticles.js
+++ Backend/gemini/getArticles.js
@@ -1,7 +1,9 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const { renderMarkdown } = require("../utils/renderMd");
 
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_KEY);
+
 
 
 async function getArticles(keywords){
--- Email-Server/database/ConnectDb.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-async function connectDB(){
-    return mongoose.connect(process.env.MONGO_URI
-        ).then(()=>console.log("Database Connected")).catch(err=>console.log("Error connecting the DB"))
-}
-
-module.exports = connectDB;
